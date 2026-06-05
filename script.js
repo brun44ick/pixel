d7fd0ddbbc80222cd96e59ed97a43ac426d89520
brun44ick <choocks@bk.ru>
2026-06-05 21:18:01 +0300

    Добавлена подсветка и новые функции

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let instruments = document.querySelector(".instruments")
 let grid = document.querySelector(".grid")
-let color = document.querySelector('input')
-
-let currColor = '#e2139a'
-let PENDOWN = false
-color.addEventListener("input", (e) => { currColor = e.target.value}, false);
 
 const w=40 
 const h=20
+
+let PENDOWN = false
+
+let instruments = document.querySelectorAll(".tool") 
+
+let color = document.querySelector('input')
+let pen = document.getElementById("pen")
+let eraser = document.getElementById("eraser")
+let fill = document.getElementById("fill")
+let trash = document.getElementById("trash")
+
+let currColor = '#e2139a'
+let currInstrument = 'pen'
+let eraseColor = "rgba(30, 41, 59, 0.8)"
+
+color.addEventListener("input", (e) => { currColor = e.target.value}, false);
+pen.addEventListener('click', function() { currInstrument = "pen"; select()});
+eraser.addEventListener('click', function() { currInstrument = "eraser"; select()});
+fill.addEventListener('click', function() { currInstrument = "fill"; select()});
+trash.addEventListener('click', function() {
+    currInstrument = "trash"; 
+    select()
+    if (confirm("Вы уверены?")) {
+        trashed()
+    }
+});
 
 function createGrid(w,h) {
     grid.style.gridTemplateColumns = `repeat(${w}, 0.6fr)`
@@ -18,12 +38,24 @@ function createGrid(w,h) {
             let poxel = document.createElement("div")
             poxel.classList.add("poxel")
             poxel.id = `px${i}-${j}`
+
             poxel.addEventListener('click', function() {
-                poxel.style.backgroundColor = currColor
-            })
-            poxel.addEventListener('mouseover', function() {
-                if (PENDOWN) {
+
+                if (currInstrument == "pen") {
                     poxel.style.backgroundColor = currColor
+                }
+                if (currInstrument == "eraser") {
+                    poxel.style.backgroundColor = eraseColor
+                }
+            })
+
+            poxel.addEventListener('mouseover', function() {
+
+                if (PENDOWN && currInstrument == "pen") {
+                    poxel.style.backgroundColor = currColor
+                }
+                if (PENDOWN && currInstrument == "eraser") {
+                    poxel.style.backgroundColor = eraseColor
                 }
             })
             grid.appendChild(poxel)
@@ -31,6 +63,42 @@ function createGrid(w,h) {
     }
 }
 
+function select() {
+    for (let tool of instruments) {
+        tool.classList.remove('selected')
+    }
+    switch (currInstrument) {
+        case "pen":
+            pen.classList.add('selected')
+            break
+        case "eraser":
+            eraser.classList.add('selected')
+            break
+        case "fill":
+            fill.classList.add('selected')
+            break
+    }
+}
+
+function filling() {
+    if (currInstrument == "fill") {
+        let poxels = document.querySelectorAll(".poxel")
+        for (let p of poxels) {
+            p.style.backgroundColor = currColor
+        }
+    }
+}
+
+function trashed() {
+    if (currInstrument == 'trash') {
+        let poxels = document.querySelectorAll(".poxel")
+        for (let p of poxels) {
+            p.style.backgroundColor = eraseColor
+        }
+    }
+}
+
 createGrid(w,h)
 grid.addEventListener('mousedown',()=> {PENDOWN = true})
 grid.addEventListener('mouseup',()=> {PENDOWN = false})
+grid.addEventListener('click', filling)
